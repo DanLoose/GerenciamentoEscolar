2157f03df038df9b294611c33a01c827452e2c4f
Danilo <danloose14@gmail.com>
2022-02-01 10:12:06 -0300

    database connection implemented

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+//  === DATABSE CONNECTION === 
+const connection = require("./database/database");
+connection.authenticate().then(() => {
+    console.log("Connection OK");
+}).catch(err => {
+    console.log(err);
+});
+
 app.get("/", (req, res) => {
     res.send("hello world!");
 });
